07062665a363e3da962bb6bfca34dbd0ba91dab0
robbiepc30 <robbiepc30@users.noreply.github.com>
2016-08-11 15:29:44 -0800

    fixed Remove-Boxstarter script

--- scripts/Remove-Boxstarter.ps1
+++ scripts/Remove-Boxstarter.ps1
@@ -2,5 +2,5 @@ $ErrorActionPreference = "Stop"
 
 choco uninstall boxstarter --allversions
 
-$path = $env:USERPROFILE\Desktop\Boxstarter Shell.lnk
+$path = "$env:USERPROFILE\Desktop\Boxstarter Shell.lnk"
 if (Test-Path $path) { Remove-Item -Path $path -Force }
