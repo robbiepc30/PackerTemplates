69fd6908c53e769d53267c2f59ef6dd5969a990a
robbiepc30 <robbiepc30@users.noreply.github.com>
2016-08-15 15:56:16 -0800

    Test install WMF5 after initial sysprep, not using box starter

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -8,5 +8,12 @@ Get-Boxstarter -Force
 $secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("vagrant", $secpasswd)
 
+# Install latest version of PowerShell (currently v5) from chocolatey
+# cannot using packer provisioning because it uses WinRM ... cannot install from WinRM, so this needs to be done before provisioning.
+# REF http://www.hurryupandwait.io/blog/safely-running-windows-automation-operations-that-typically-fail-over-winrm-or-powershell-remoting
+# Also after much trial and error there are issues installing it using Boxstarter, so doing it here instead
+
+choco install powershell -y 
+
 Import-Module $env:appdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
 Install-BoxstarterPackage -PackageName a:\package.ps1 -Credential $cred
