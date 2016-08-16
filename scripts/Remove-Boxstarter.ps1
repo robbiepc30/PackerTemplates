45b4b45f2973f3eacaf50b01db2f286e52041730
robbiepc30 <robbiepc30@users.noreply.github.com>
2016-08-16 11:28:59 -0800

    Revert "Trying to reset baseline... trial and error"
    This reverts commit 7c684e3833c1c6de6ab1d6e828091632cfbafe9f.

---
+++ scripts/Remove-Boxstarter.ps1
@@ -0,0 +1,6 @@
+$ErrorActionPreference = "Stop"
+
+choco uninstall boxstarter --allversions
+
+$path = "$env:USERPROFILE\Desktop\Boxstarter Shell.lnk"
+if (Test-Path $path) { Remove-Item -Path $path -Force }
---
+++ scripts/Remove-Chocolatey.ps1
@@ -0,0 +1,17 @@
+$ErrorActionPreference = "Stop"
+
+Remove-Item -Recurse -Force "$env:ChocolateyInstall"
+[System.Text.RegularExpressions.Regex]::Replace([Microsoft.Win32.Registry]::CurrentUser.OpenSubKey('Environment').GetValue('PATH', '', [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames).ToString(), [System.Text.RegularExpressions.Regex]::Escape("$env:ChocolateyInstall\bin") + '(?>;)?', '', [System.Text.RegularExpressions.RegexOptions]::IgnoreCase) | %{[System.Environment]::SetEnvironmentVariable('PATH', $_, 'User')}
+
+
+[System.Text.RegularExpressions.Regex]::Replace([Microsoft.Win32.Registry]::LocalMachine.OpenSubKey('SYSTEM\CurrentControlSet\Control\Session Manager\Environment\').GetValue('PATH', '', [Microsoft.Win32.RegistryValueOptions]::DoNotExpandEnvironmentNames).ToString(), [System.Text.RegularExpressions.Regex]::Escape("$env:ChocolateyInstall\bin") + '(?>;)?', '', [System.Text.RegularExpressions.RegexOptions]::IgnoreCase) | %{[System.Environment]::SetEnvironmentVariable('PATH', $_, 'Machine')}
+
+if ($env:ChocolateyBinRoot -ne '' -and $env:ChocolateyBinRoot -ne $null) { Remove-Item -Recurse -Force "$env:ChocolateyBinRoot" }
+if ($env:ChocolateyToolsRoot -ne '' -and $env:ChocolateyToolsRoot -ne $null) { Remove-Item -Recurse -Force "$env:ChocolateyToolsRoot" }
+
+# Remove Environment Variables
+[System.Environment]::SetEnvironmentVariable("ChocolateyBinRoot", $null, 'User')
+[System.Environment]::SetEnvironmentVariable("ChocolateyToolsLocation", $null, 'User')
+
+[System.Environment]::SetEnvironmentVariable("ChocolateyInstall", $null, 'User')
+[System.Environment]::SetEnvironmentVariable("ChocolateyLastPathUpdate", $null, 'User')
