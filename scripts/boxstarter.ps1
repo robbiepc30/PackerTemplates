d81cbd7d06a80802f962571634f706e2ea2a47e9
robbiepc30 <robbiepc30@users.noreply.github.com>
2016-08-17 14:36:32 -0800

    Finally got this to work with vagrant after installing WMF5
    noticed I need to add some stuff to the Remove-Boxstarter.ps1 to remove
    some stuff in the %appadata% folder

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -5,8 +5,29 @@ Remove-ItemProperty -Path $WinlogonPath -Name DefaultUserName
 iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/mwrock/boxstarter/master/BuildScripts/bootstrapper.ps1'))
 Get-Boxstarter -Force
 
-$secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ("vagrant", $secpasswd)
+# Testing something by moving this: 
+# It seems that if WMF5 is installed and then rebooted (now PS is running v5) and then the WinRM stuff is enabled, vagrant errors out with WinRM::WinRMAuthorizationError
+# however if these settings are applied first then WMF5 is intalled it works fine... I have no idea why... Need to investgate further...
+Write-Host "Setting up WinRM"
+#block WinRM for now. need to hold off untill boxstarter is finished, then packer provisioning will take over
+netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=block
+$enableArgs=@{Force=$true}
+try {
+ $command = Get-Command Enable-PSRemoting
+  if ($command.Parameters.Keys -contains "skipnetworkprofilecheck") {
+      $enableArgs.skipnetworkprofilecheck=$true
+  }
+}
+catch {
+  $global:error.RemoveAt(0)
+}
+
+Enable-PSRemoting @enableArgs
+Enable-WSManCredSSP -Force -Role Server
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+Write-Host "winrm setup complete... with exception of firewall rule, blocking untill boxstarter finishes setup"
 
 # Install latest version of PowerShell (currently v5) from chocolatey
 # cannot using packer provisioning because it uses WinRM ... cannot install from WinRM, so this needs to be done before provisioning.
@@ -15,5 +36,11 @@ $cred = New-Object System.Management.Automation.PSCredential ("vagrant", $secpas
 
 choco install powershell -y 
 
+$secpasswd = ConvertTo-SecureString "vagrant" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential ("vagrant", $secpasswd)
 Import-Module $env:appdata\boxstarter\boxstarter.chocolatey\boxstarter.chocolatey.psd1
 Install-BoxstarterPackage -PackageName a:\package.ps1 -Credential $cred
+
+# Testing something by using -DisableReboots
+# It seems that if WMF5 is installed and then rebooted (now PS is running v5) and then the WinRM stuff is enabled, vagrant errors out with WinRM::WinRMAuthorizationError
+# however if these settings are applied first then WMF5 is intalled it works fine... I have no idea why... Need to investgate further...
--- scripts/package.ps1
+++ scripts/package.ps1
@@ -16,7 +16,7 @@ Write-BoxstarterMessage "Enable Remote Desktop"
 Enable-RemoteDesktop
 netsh advfirewall firewall add rule name="Remote Desktop" dir=in localport=3389 protocol=TCP action=allow
 
-Write-BoxstarterMessage "Enabling file sharing firewale rules"
+Write-BoxstarterMessage "Enabling file sharing firewall rules"
 netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=yes
 
 Update-ExecutionPolicy -Policy Unrestricted
@@ -33,29 +33,7 @@ Update-ExecutionPolicy -Policy Unrestricted
 #Write-BoxstarterMessage "Installing Windows Updates"
 #Install-WindowsUpdate -AcceptEula
 
-Write-BoxstarterMessage "Setting up WinRM"
-
-$enableArgs=@{Force=$true}
-try {
- $command = Get-Command Enable-PSRemoting
-  if ($command.Parameters.Keys -contains "skipnetworkprofilecheck") {
-      $enableArgs.skipnetworkprofilecheck=$true
-  }
-}
-catch {
-  $global:error.RemoveAt(0)
-}
-
-Enable-PSRemoting @enableArgs
-Enable-WSManCredSSP -Force -Role Server
-winrm set winrm/config/client/auth '@{Basic="true"}'
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-
-# Testing something by moving this:  if(Test-PendingReboot){ Invoke-Reboot }  to the end of the script
-# It seems that if WMF5 is installed and then rebooted (now PS is running v5) and then the WinRM stuff is enabled, vagrant errors out with WinRM::WinRMAuthorizationError
-# however if these settings are applied first then WMF5 is intalled it works fine... I have no idea why... Need to investgate further...
 if(Test-PendingReboot){ Invoke-Reboot }
 
-netsh advfirewall firewall add rule name="WinRM-HTTP" dir=in localport=5985 protocol=TCP action=allow
-Write-BoxstarterMessage "winrm setup complete"
+Write-BoxstarterMessage "Enable WinRM firewall rule"
+netsh advfirewall firewall set rule name="WinRM-HTTP" new action=allow
